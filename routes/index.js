c4569b1a90ce2067f91e3919ae18ad81fb6a2ce6
Shribalaji Ganesan <shri@virtuatechnologies.in>
2019-10-15 15:34:25 +0530

    debugs are added

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const index_ctrl = require('../controllers/index')
+var debug = require('debug')('girlfriday:server');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -29,6 +30,7 @@ router.get('/checkapp', async function(req, res, next) {
 
 router.get("/start_bot", async function(req, res) {
   try {
+    debug('/start_bot init');
     let result = await index_ctrl.send_welcome_note(req.body);
     let send_data = {
       success: true,
@@ -37,6 +39,7 @@ router.get("/start_bot", async function(req, res) {
     };
     return res.json(send_data);
   } catch (error) {
+     debug('/start_bot exception');
      // Error Web Response
      let send_data = {
       success: false,
